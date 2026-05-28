faaddb14706186dc2b57a210a68458271ab5933a
Asildar-Hajiyev <tu201806131@code.edu.az>
2026-05-29 01:31:48 +0400

    Up Sec 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Home from "./Home"
+import Footer from './Footer'
 
 function App() {
   return (
     <>
       <Header/>
       <Home/>
+      <Footer/>
     </>
   )
 }
--- src/Section8.jsx
+++ src/Section8.jsx
@@ -2,7 +2,7 @@ import cta from './assets/cta.svg'
 function Section8() {
   return (
      <section className='py-10'>
-      <div className='container mx-auto px-6 flex flex-col sm:flex-row items-center justify-center gap-8'>
+      <div className='container mx-auto px-6 flex flex-col sm:flex-row items-center justify-center gap-8 shadow-sm rounded-2xl'>
         <img src={cta} alt="" className="w-[70%] sm:w-[50%] lg:w-[30%]"/>
         <div className='flex items-center lg:items-start flex-col gap-3 text-center lg:text-left'>
           <h2 className='font-semibold text-2xl sm:text-3xl'>Ready to get started?</h2>
