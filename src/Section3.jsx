ebb39ae50a78f452e6a557797d688f92029bd28d
Asildar-Hajiyev <tu201806131@code.edu.az>
2026-05-28 23:43:23 +0400

    End update Section 3

--- src/Section3.jsx
+++ src/Section3.jsx
@@ -1,9 +1,13 @@
-import { FaLongArrowAltRight } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa6";
+
 import slide1 from './assets/service-slide-1.png'
 import slide2 from './assets/service-slide-2.png'
 import slide3 from './assets/service-slide-3.png'
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+import "swiper/css/autoplay";
+
 import "swiper/css";
 import "swiper/css/pagination";
 function Section3() {
@@ -19,10 +23,10 @@ function Section3() {
             elit. Consequat tristique eget amet, tempus eu at consecttur. Leo
             facilisi nunc viverra tellus. Ac laoreet sit vel consquat.
           </p>
-          <a href="#" className="flex items-center gap-1 ">Check it out <FaLongArrowAltRight/></a>
+          <a href="#" className="flex items-center gap-1 text-[#00ABAA]">Check it out <FaArrowRight/></a>
         </div>
          <div className="w-full lg:w-[45%]">
-            <Swiper modules={[Pagination]} pagination={{ clickable: true }}>
+            <Swiper modules={[Pagination, Autoplay ]} pagination={{ clickable: true }}  autoplay={{ delay: 3000 }}>
                 <SwiperSlide><img src={slide1} alt="" className="w-full rounded-xl" /></SwiperSlide>
                 <SwiperSlide><img src={slide2} alt="" className="w-full rounded-xl" /></SwiperSlide>
                 <SwiperSlide><img src={slide3} alt="" className="w-full rounded-xl" /></SwiperSlide>
